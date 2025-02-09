dff4de7146d6ebd42a574efc014cc217c4d77b07
thestrangerd <ashleyseto25@gmail.com>
2025-02-09 02:47:29 -0800

    transitional animation

--- madlibs/v2/script.js
+++ madlibs/v2/script.js
@@ -5,6 +5,14 @@
     'use strict';
     console.log('reading js');
 
-     
+    document.querySelector('#submit').addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const screen1 = document.querySelector('#screen1');
+        const screen2 = document.querySelector('#screen2');
+
+        screen1.className = "screen hidden";
+        screen2.className = "screen showing";
+    });
 
 })();
