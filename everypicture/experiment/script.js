d1ff2f9d578ebb5fe8b1f8b5d683544232986df2
thestrangerd <ashleyseto25@gmail.com>
2025-02-17 23:41:23 -0800

    more epp exp

--- everypicture/experiment/script.js
+++ everypicture/experiment/script.js
@@ -6,18 +6,47 @@
     console.log('reading js');
 
 
-    const image = document.querySelector('img');
+    // CONVEYOR BELT
+    const stuffie = document.querySelectorAll('.stuffie');
 
-    image.addEventListener('mouseover', function(event) {
-        event.preventDefault();
-        image.style.transofmr = 'scale(1.5)';
+    stuffie.forEach(function(images) {
+
+        images.addEventListener('mouseover', function(){
+            let glowColor = '';
+
+            if (images.classList.contains('bunbun')) {
+                glowColor = 'hotpink';
+            } else if (images.classList.contains('bolt')) {
+                glowColor = 'skyblue';
+            } else if (images.classList.contains('bunny')) {
+                glowColor = 'lightgreen';
+            } else if (images.classList.contains('peppers')) {
+                glowColor = 'orange';
+            } else if (images.classList.contains('penguin')) {
+                glowColor = 'rgb(124, 18, 124)';
+            } 
+
+            images.style.filter = `drop-shadow(0 0 8px ${glowColor}) grayscale(0%) saturate(1.2)`;
+            images.style.cursor = 'pointer';
+            images.style.transform = 'scale(1.2)';
+        });
+
+        images.addEventListener('mouseleave', function(){
+            images.style.filter = 'drop-shadow(0 0 3px white) grayscale(100%)';
+            images.style.transform = 'scale(1)';
+        });
     });
 
-    image.addEventListener('mouseleave', function(event) {
-        event.preventDefault();
-        image.style.transofmr = 'scale(1)';
-    });
-    
+
+
+    // ON REFRESH CART ANIMATION ACTIVATES
+
+    const cart = document.querySelector('#cart');
+
+    window.onload = function() {
+        console.log('cart moves in');
+        cart.style.animation = 'cartMove 2s ease forwards'
+    }
 
 
 })();
