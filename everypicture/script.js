82d34bc75b81653709c832b831755ac61d99011f
thestrangerd <ashleyseto25@gmail.com>
2025-02-20 14:08:54 -0800

    epp

---
+++ everypicture/script.js
@@ -0,0 +1,72 @@
+
+
+
+(function(){
+    'use strict';
+    console.log('reading js');
+
+
+    // FLIPPING PAGES WITH ARROWS
+
+    const body = document.querySelector('body');
+    const nextButton = document.querySelector('#next');
+    const prevButton = document.querySelector('#previous');
+    const nextText = document.querySelector('#nexttext');
+    const prevText = document.querySelector('#previoustext');
+
+    nextButton.addEventListener('click', nextPage);
+    prevButton.addEventListener('click', previousPage);
+    let currentPage = 0;
+
+    const pages = [
+        'url(images/bg-cover.svg)',
+        'url(images/bg-pg1.svg)',
+        'url(images/bg-pg2.svg)',
+        'url(images/bg-pg3.svg)',
+        'url(images/bg-pg4.svg)',
+        'url(images/bg-pg5.svg)',
+    ]
+
+    updatePage();
+
+    function nextPage() {
+        if (currentPage < pages.length - 1) {
+            currentPage++;
+            updatePage();
+        }
+    }
+
+    function previousPage() {
+        if (currentPage > 0) {
+            currentPage--;
+            updatePage();
+        }
+    }
+
+    function updatePage() {
+        // CHANGES BACKGROUND ON BUTTON CLICK
+        body.style.background = `${pages[currentPage]} no-repeat center center fixed`;
+        body.style.backgroundSize = 'cover';
+
+        console.log('previous page');
+        console.log('next page');
+
+        // HIDES ARROWS ON FIRST AND LAST PAGE
+        if (currentPage === 0) {
+            prevButton.className = 'arrow hidden';
+            prevText.className = 'previoustext hidden'
+        } else {
+            prevButton.className = 'arrow showing';
+            prevText.className = 'previoustext'
+        }
+
+        if (currentPage === pages.length - 1) {
+            nextButton.className = 'arrow hidden';
+            nextText.className = 'nexttext hidden'
+        } else {
+            nextButton.className = 'arrow showing';
+            nextText.className = 'nexttext'
+        }
+    }
+
+})();
