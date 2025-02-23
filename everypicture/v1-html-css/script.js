bc0c313fdf3bd09f0fa32ad94acbf17f4276448b
thestrangerd <ashleyseto25@gmail.com>
2025-02-23 03:14:10 -0800

    epp pages

--- everypicture/v1-html-css/script.js
+++ everypicture/v1-html-css/script.js
@@ -8,65 +8,74 @@
 
     // FLIPPING PAGES WITH ARROWS
 
-    // const body = document.querySelector('body');
-    // const nextButton = document.querySelector('#next');
-    // const prevButton = document.querySelector('#previous');
-    // const nextText = document.querySelector('#nexttext');
-    // const prevText = document.querySelector('#previoustext');
+    const bookPage = document.querySelector('.cover');
+    const nextButton = document.querySelector('#next');
+    const prevButton = document.querySelector('#previous');
+    const nextText = document.querySelector('#nexttext');
+    const prevText = document.querySelector('#previoustext');
 
-    // nextButton.addEventListener('click', nextPage);
-    // prevButton.addEventListener('click', previousPage);
-    // let currentPage = 0;
+    nextButton.addEventListener('click', nextPage);
+    prevButton.addEventListener('click', previousPage);
+    let currentPage = 0;
 
-    // const pages = [
-    //     'url(images/bg-cover.svg)',
-    //     'url(images/bg-pg1.svg)',
-    //     'url(images/bg-pg2.svg)',
-    //     'url(images/bg-pg3.svg)',
-    //     'url(images/bg-pg4.svg)',
-    //     'url(images/bg-pg5.svg)',
-    // ]
+    const pages = [
+        'images/cover.svg',
+        'images/page1.svg',
+        'images/page2.svg',
+        'images/page3.svg',
+        'images/page4.svg',
+        'images/page5.svg',
+        'images/back-cover.svg'
+    ]
 
-    // updatePage();
+    function nextPage() {
+        // ON BUTTON CLICK, INCREASE IMG IN ARRAY
+        console.log('next button clicked');
+        if (currentPage < pages.length - 1) {
+            currentPage++;
+            updatePage();
+        }
+    }
 
-    // function nextPage() {
-    //     if (currentPage < pages.length - 1) {
-    //         currentPage++;
-    //         updatePage();
-    //     }
-    // }
+    function previousPage() {
+        // ON BUTTON CLICK, DECREASE IMG IN ARRAY
+        console.log('last button clicked');
+        if (currentPage > 0) {
+            currentPage--;
+            updatePage();
+        }
+    }
 
-    // function previousPage() {
-    //     if (currentPage > 0) {
-    //         currentPage--;
-    //         updatePage();
-    //     }
-    // }
+    function updatePage() {
+        // CHANGES PAGE SVG ON BUTTON CLICK
+        bookPage.src = pages[currentPage];
 
-    // function updatePage() {
-    //     // CHANGES BACKGROUND ON BUTTON CLICK
-    //     body.style.background = `${pages[currentPage]} no-repeat center center fixed`;
-    //     body.style.backgroundSize = 'cover';
+        if (currentPage === 0) {
+            bookPage.className = 'cover';
+        } else if (currentPage === pages.length - 1) {
+            bookPage.className = 'back';
+        } else {
+            bookPage.className = 'page';
+        }
 
-    //     console.log('previous page');
-    //     console.log('next page');
+        // HIDES ARROWS ON FIRST AND LAST PAGE
+        if (currentPage === pages.length - 1) {
+            nextButton.className = 'arrow hidden';
+            nextText.className = 'nexttext hidden'
+        } else {
+            nextButton.className = 'arrow showing';
+            nextText.className = 'nexttext'
+        }
 
-    //     // HIDES ARROWS ON FIRST AND LAST PAGE
-    //     if (currentPage === 0) {
-    //         prevButton.className = 'arrow hidden';
-    //         prevText.className = 'previoustext hidden'
-    //     } else {
-    //         prevButton.className = 'arrow showing';
-    //         prevText.className = 'previoustext'
-    //     }
+        if (currentPage === 0) {
+            prevButton.className = 'arrow hidden';
+            prevText.className = 'previoustext hidden'
+        } else {
+            prevButton.className = 'arrow showing';
+            prevText.className = 'previoustext'
+        }
+    }
 
-    //     if (currentPage === pages.length - 1) {
-    //         nextButton.className = 'arrow hidden';
-    //         nextText.className = 'nexttext hidden'
-    //     } else {
-    //         nextButton.className = 'arrow showing';
-    //         nextText.className = 'nexttext'
-    //     }
-    // }
+    updatePage();
 
 })();
