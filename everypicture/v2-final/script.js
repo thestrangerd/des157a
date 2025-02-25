6c57f6411c577d6ebc2ba41ffb41a85f2b20a339
thestrangerd <ashleyseto25@gmail.com>
2025-02-25 13:30:16 -0800

    added all stuffie images

--- everypicture/v2-final/script.js
+++ everypicture/v2-final/script.js
@@ -129,4 +129,36 @@
         }
     });
 
+
+
+    // STUFFED ANIMAL HOVER COLORS 
+
+    const stuffie = document.querySelectorAll('.stuffie');
+
+    stuffie.forEach(function(images) {
+
+        images.addEventListener('mouseover', function(){
+            let glowColor = '';
+
+            if (images.classList.contains('bunbun')) {
+                glowColor = 'hotpink';
+            } else if (images.classList.contains('bolt')) {
+                glowColor = 'skyblue';
+            } else if (images.classList.contains('bunny')) {
+                glowColor = 'lightgreen';
+            } else if (images.classList.contains('peppers')) {
+                glowColor = 'orange';
+            } else if (images.classList.contains('penguin')) {
+                glowColor = 'rgb(124, 18, 124)';
+            } 
+
+            images.style.filter = `drop-shadow(0 0 8px ${glowColor}) grayscale(0%) saturate(1.2)`;
+            images.style.cursor = 'pointer';
+        });
+
+        images.addEventListener('mouseleave', function(){
+            images.style.filter = 'drop-shadow(0 0 3px white) grayscale(100%)';
+        });
+    });
+
 })();
