fd6fafb7b4cfbdd48e0973f04f90cb3f78efc5ec
thestrangerd <ashleyseto25@gmail.com>
2025-02-25 11:40:10 -0800

    adding page content

--- everypicture/v2-final/script.js
+++ everypicture/v2-final/script.js
@@ -6,13 +6,12 @@
     console.log('reading js');
 
 
-    // FLIPPING PAGES WITH ARROWS
-
     const bookPage = document.querySelector('.cover');
     const nextButton = document.querySelector('#next');
     const prevButton = document.querySelector('#previous');
     const nextText = document.querySelector('#nexttext');
     const prevText = document.querySelector('#previoustext');
+    const allPages = document.querySelectorAll('.page-content');
 
     nextButton.addEventListener('click', nextPage);
     prevButton.addEventListener('click', previousPage);
@@ -28,9 +27,12 @@
         'images/back-cover.svg'
     ]
 
+
+
+    // FLIPPING PAGES WITH ARROWS
+
     function nextPage() {
-        // ON BUTTON CLICK, INCREASE IMG IN ARRAY
-        console.log('next button clicked');
+        // on button click, increase img in array
         if (currentPage < pages.length - 1) {
             currentPage++;
             updatePage();
@@ -38,18 +40,36 @@
     }
 
     function previousPage() {
-        // ON BUTTON CLICK, DECREASE IMG IN ARRAY
-        console.log('last button clicked');
+        // on button click, decrease img in array
         if (currentPage > 0) {
             currentPage--;
             updatePage();
         }
     }
 
-    function updatePage() {
-        // CHANGES PAGE SVG ON BUTTON CLICK
-        bookPage.src = pages[currentPage];
 
+
+    // UPDATE PAGE CONTENT
+
+    function updatePage() {
+        // initially hides all page content
+        for (let i = 0; i < allPages.length; i++) {
+            allPages[i].className = 'page-content hidden';
+        }
+
+        // detects page # in array and shows page content
+        if (currentPage > 0 && currentPage <= allPages.length) {
+            const currentPageContent = document.querySelector(`#page${currentPage}`);
+            if (currentPageContent) {
+                currentPageContent.className = 'page-content showing';
+            } 
+        }
+
+        // change page svg on button click
+        bookPage.src = pages[currentPage];
+        console.log(`#page${currentPage}`)
+
+        // determines style / position based on which page
         if (currentPage === 0) {
             bookPage.className = 'cover';
         } else if (currentPage === pages.length - 1) {
@@ -58,24 +78,55 @@
             bookPage.className = 'page';
         }
 
-        // HIDES ARROWS ON FIRST AND LAST PAGE
-        if (currentPage === pages.length - 1) {
-            nextButton.className = 'arrow hidden';
-            nextText.className = 'nexttext hidden'
-        } else {
-            nextButton.className = 'arrow showing';
-            nextText.className = 'nexttext'
-        }
-
+        // hides arrows on first and last page
         if (currentPage === 0) {
+            // hides prev arrow on cover page
             prevButton.className = 'arrow hidden';
             prevText.className = 'previoustext hidden'
         } else {
             prevButton.className = 'arrow showing';
             prevText.className = 'previoustext'
         }
+
+        if (currentPage === pages.length - 1) {
+            // hides next arrow on last page
+            nextButton.className = 'arrow hidden';
+            nextText.className = 'nexttext hidden'
+        } else {
+            nextButton.className = 'arrow showing';
+            nextText.className = 'nexttext'
+        }
     }
 
     updatePage();
+
+
+
+    // SIDE TAB (KEY WORDS)
+
+    const sideTab = document.querySelector('#side-tab');
+    const sidePanel = document.querySelector('#side-panel');
+
+    // open panel by clicking tab or tab p
+    sideTab.addEventListener('click', function(event) {
+        event.preventDefault()
+
+        if (sidePanel.className === 'open') {
+            sideTab.className = 'close';
+            sidePanel.className = 'close';
+        } else {
+            sideTab.className = 'open';
+            sidePanel.className = 'open';
+        }
+    }); 
+
+    // closes tab with escape key
+    document.addEventListener('keydown', function(event) {
+        event.preventDefault();
+        if (event.key === 'Escape') {
+            sideTab.className = 'close';
+            sidePanel.className = 'close';
+        }
+    });
 
 })();
