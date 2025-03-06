367ff79b1a7e0e073895ce032c8afa0ac40ba1de
thestrangerd <ashleyseto25@gmail.com>
2025-03-06 13:24:36 -0800

    fixed title screen

--- game/v1-html-css-only/script.js
+++ game/v1-html-css-only/script.js
@@ -5,9 +5,27 @@
     'use strict';
     console.log('reading js');
 
+
+    const title = document.querySelector('#title-screen');
+    const game = document.querySelector('#game-container');
+
+
+    // ON START BUTTON CLICK, FADE GAME IN
     document.querySelector('#start-button').addEventListener('click', function(){
-        document.querySelector('#title-screen').style.display = 'none';
-        document.querySelector('#game-container').style.display = 'block';
+
+        // hide title screen
+        title.style.opacity = 0;
+
+        setTimeout(function() {
+            title.style.display = 'none';
+        }, 1000);
+
+        // show game 
+        setTimeout(function() {
+            game.style.opacity = 1;
+            game.style.display = 'block';
+        }, 100);
+        
     });
 
 })();
