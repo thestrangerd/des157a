deed1c69acbac220138eb56cc438a35ad4f60311
thestrangerd <ashleyseto25@gmail.com>
2025-03-13 13:31:46 -0700

    game movement

--- game/v2-final/script.js
+++ game/v2-final/script.js
@@ -15,17 +15,21 @@
     const overlay = document.querySelector('.overlay');
     const closeBtn = document.querySelector('#close');
 
+    const capy1 = document.querySelector('#capy1');
+    const capy2 = document.querySelector('#capy2');
+    const swimBtn = document.querySelector('#swim-button');
+
 
     const gameData = {
-        dice: ['1die.jpg', '2die.jpg', '3die.jpg', 
-                '4die.jpg', '5die.jpg', '6die.jpg'],
-        players: ['Player 1', 'Player 2'],
+        players: ['Capy 1', 'Capy 2'],
         score: [0, 0],
         roll1: 0,
         roll2: 0,
         rollSum: 0,
         index: 0, // keeps track of player currently playing 0-1
-        gameEnd: 29
+        gameEnd: 49,
+        capy1Position: 0,
+        capy2Position: 0
     }
 
 
@@ -75,8 +79,83 @@
 
     function setUpTurn() {
         text.innerHTML = `It's ${gameData.players[gameData.index]}'s Turn!`
+
+        swimBtn.addEventListener('click', function() {
+            throwDice();
+        })
     }
 
+
+
+    function throwDice() {
+        // randomize rolls
+        gameData.roll1 = Math.floor(Math.random() * 6) + 1;
+        gameData.roll2 = Math.floor(Math.random() * 6) + 1;
+
+        const skipLines = [
+            `${gameData.players[gameData.index]} gets distracted by the splashing! Switch players.`,
+            `${gameData.players[gameData.index]} is too busy playing in the water! Switch players.`,
+            `${gameData.players[gameData.index]} feels bored swimming. Switch players.`,
+            `${gameData.players[gameData.index]} needs a little rest. Switch players.`,
+            `${gameData.players[gameData.index]} wonders if the effort is worth it. Switch players.`,
+        ];
+
+        // move capybaras depending on roll
+        text.innerHTML = `${gameData.players[gameData.index]}, feels determined. Keep swimming!`;
+
+
+        // if either roll = 1 then skip player
+        if (gameData.roll1 === 1 || gameData.roll2 === 1) {
+            // randomize skip lines in array
+            const distracted = skipLines[Math.floor(Math.random() * skipLines.length)];
+            text.innerHTML = distracted;
+            
+            // prevents button use when switching players
+            swimBtn.disabled = true;
+
+            setTimeout(function() {
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                console.log(`Switched to player: ${gameData.players[gameData.index]}`);
+                console.log(gameData.roll1, gameData.roll2);
+
+                swimBtn.disabled = false;
+                setUpTurn();
+            }, 1000);
+            
+        }
+        else {
+            // if neither = 1, update distance of current player
+            movePlayer();
+            gameData.score[gameData.index] += gameData.rollSum;
+        }
+
+        checkWinningCondition();
+    }
+
+
+
+    function movePlayer() {
+        // move player right by 20px
+        const moveAmount = 25;
+
+        if (gameData.index === 0) {
+            gameData.capy1Position += moveAmount;
+            capy1.style.transform = `translateX(${gameData.capy1Position}px)`;
+        } else {
+            gameData.capy2Position += moveAmount;
+            capy2.style.transform = `translateX(${gameData.capy2Position}px)`;
+        }
+    }
+
+
+
+    function checkWinningCondition() {
+        if (gameData.score[gameData.index] >= gameData.gameEnd) {
+            // win screen shows up
+            // text shows which capy won
+            // button starts a new game
+        }
+    }
 
 
 })();
